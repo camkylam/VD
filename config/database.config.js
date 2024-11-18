f58910f96c9c50ecc9788edd678c2678644eeb76
unknown <camkylam@gmail.com>
2024-11-18 16:06:20 +0700

    Cập nhật cấu hình database và chỉnh sửa model user

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/users');
+        await mongoose.connect('mongodb+srv://lcky:YykC3TJZhY@cluster0.skjwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
--- models/user.model.js
+++ models/user.model.js
@@ -7,4 +7,4 @@ const userSchema = new mongoose.Schema({
     address: {type: String, required:true}
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Data', userSchema);
